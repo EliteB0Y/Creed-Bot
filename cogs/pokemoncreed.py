776673bb02c48612c53b7a4a54c6297a2d4daa2b
EliteB0Y <46435380+EliteB0Y@users.noreply.github.com>
2026-06-04 11:31:42 +0530

    Update mystbin function to use pastey.gg API

--- cogs/pokemoncreed.py
+++ cogs/pokemoncreed.py
@@ -10,15 +10,15 @@ class BoxMenu(menus.Menu):
     
     async def mystbin(self, text):
         async with aiohttp.ClientSession() as session:
-            payload = {"files":[{"content": text, "filename": "No Title"}]}
+            payload = {"files":[{"content": text, "name": "No Title"}]}
             headers = {"Content-Type": "application/json"}
-            async with session.post(url="https://mystbin.abstractumbra.dev/api/paste", json=payload, headers=headers) as r:
-                if r.status == 200:
+            async with session.post(url="https://api.pastey.gg/pastes", json=payload, headers=headers) as r:
+                if r.status == 201:
                     mdata = await r.json()
-                    return f"https://mystbin.abstractumbra.dev/{mdata['id']}"
+                    return f"https://pastey.gg/{mdata['id']}"
                 else:
-                    print("Oops! I couldn't upload text to mystb.in")
-                    return f"https://mystbin.abstractumbra.dev"
+                    print("Oops! I couldn't upload text to pastey.gg")
+                    return f"https://pastey.gg/"
 
     async def cleanResults(self):
         if self.results["success"]:
@@ -171,15 +171,15 @@ class PokemonCreed(commands.Cog):
 
     async def mystbin(self, text):
         async with aiohttp.ClientSession() as session:
-            payload = {"files":[{"content": text, "filename": "No Title"}]}
+            payload = {"files":[{"content": text, "name": "No Title"}]}
             headers = {"Content-Type": "application/json"}
-            async with session.post(url="https://mystbin.abstractumbra.dev/api/paste", json=payload, headers=headers) as r:
-                if r.status == 200:
+            async with session.post(url="https://api.pastey.gg/pastes", json=payload, headers=headers) as r:
+                if r.status == 201:
                     mdata = await r.json()
-                    return f"https://mystbin.abstractumbra.dev/{mdata['id']}"
+                    return f"https://pastey.gg/{mdata['id']}"
                 else:
-                    print("Oops! I couldn't upload text to mystb.in")
-                    return f"https://mystbin.abstractumbra.dev"
+                    print("Oops! I couldn't upload text to pastey.gg")
+                    return f"https://pastey.gg/"
     
     def human_format(self, num, round_to=2):
         magnitude = 0
